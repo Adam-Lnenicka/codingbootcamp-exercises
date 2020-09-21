abf07fb8269911449abe555e89211b30250f25f4
j_polak <jan.polak@onmyway.cz>
2020-09-21 15:31:59 +0200

    day 11 functions with parameters

--- day11-javascript-intro/functions.js
+++ day11-javascript-intro/functions.js
@@ -13,7 +13,16 @@ const set_headline = () => {
     h1; // the element
     h1.style; // the element's style (another object)
     h1.style.backgroundColor; // the element's style's background-color property
-    
+
     h1.style.backgroundColor = 'red';
 
+    // document.getElementById('headline').style.backgroundColor = 'blue';
+
+}
+
+const change_content = (element_id, new_contents) => {
+
+    let element = document.getElementById( element_id );
+
+    element.innerHTML = new_contents;
 }
