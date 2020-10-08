ecac09c9faffa6cd1a31f4dfe14dcd92f210c644
j_polak <jan.polak@onmyway.cz>
2020-10-08 13:25:06 +0200

    day 24 include require playground

---
+++ day24-php/include-require-playground/footer.php
@@ -0,0 +1,5 @@
+<?php require_once 'functions.php'; ?>
+
+<footer>
+    &copy; <?= get_current_year() ?> Me & my better half
+</footer>
---
+++ day24-php/include-require-playground/header.php
@@ -0,0 +1,9 @@
+<?php require_once 'functions.php'; ?>
+
+<header>
+    My amazing website (version <?= get_current_year() ?>)
+    <nav>
+        <a href="home.php">Homepage</a>
+        <a href="page.php">Subpage</a>
+    </nav>
+</header>
